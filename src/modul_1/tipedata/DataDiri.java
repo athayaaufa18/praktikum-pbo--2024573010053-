c3651d13459fc864b8672ba08ae5f92dffdbd3da
athayaaufa18 <athayaaufa58@gmail.com>
2025-09-11 16:36:59 +0700

    datadiri

---
+++ src/modul_1/tipedata/DataDiri.java
@@ -0,0 +1,26 @@
+package modul_1.tipedata;
+
+public class DataDiri {
+    public static void main(String[]args){
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        float berat;
+        char nilai;
+
+        nama = "jooyeon";
+        alamat = "Dubai";
+        tinggi =3.18;
+        usia = 22;
+        berat =35;
+        nilai = 'A';
+
+        System.out.println("nama:" + nama);
+        System.out.println("alamat:" + alamat);
+        System.out.println("tinggi:" + tinggi);
+        System.out.println("usia:" + usia);
+        System.out.println("berat:" + berat);
+        System.out.println("nilai:" + nilai);
+
+    }
+}
